fef2fdef3dee93bd0e504986815d04f953dddd0c
RomaBortnik <bortnik25051998@gmail.com>
2023-06-05 13:55:33 +0300

    added notification

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -1,17 +1,28 @@
 import TweetListItem from 'components/TweetListItem';
 import { useSelector } from 'react-redux';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { selectVisibleUsers } from 'redux/selectors';
+import { selectStatusFilter, selectVisibleUsers } from 'redux/selectors';
 import { StyledTweetList } from './TweetList.styled';
 
 const TweetList = () => {
   const users = useSelector(selectVisibleUsers);
+  const filterStatus = useSelector(selectStatusFilter);
+
+  users.length === 0 &&
+    (filterStatus === 'following' || filterStatus === 'unfollowing') &&
+    toast.warning('There are no users matching your request yet.');
+
   return (
-    <StyledTweetList>
-      {users.map(user => (
-        <TweetListItem key={user.id} userCard={user}></TweetListItem>
-      ))}
-    </StyledTweetList>
+    <>
+      <StyledTweetList>
+        {users.map(user => (
+          <TweetListItem key={user.id} userCard={user}></TweetListItem>
+        ))}
+      </StyledTweetList>
+      <ToastContainer autoClose={2000} theme="dark" />
+    </>
   );
 };
 
